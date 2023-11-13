b770dc0ad8b716875d6c763f53734a0a3594b1f0
apelliciari <apelliciari@fi.uba.ar>
2023-11-13 12:30:46 -0300

    Cambios varios y testeos de funciones del ABB

--- ayp2_plantilla_tp_grupal_2c2023-main/main.cpp
+++ ayp2_plantilla_tp_grupal_2c2023-main/main.cpp
@@ -1,3 +1,31 @@
+#include "Arbol_de_placas.hpp"
+#include <string>
+#include <iostream>
+#include "bGVjdG9y.hpp"
+
 int main() {
+
+    Arbol_de_placas arbol;
+
+    // ALTA
+    Placa* placa_aux = new Placa("X", "Y", 1024);
+    arbol.alta_placa(placa_aux);
+
+    // MENSAJE
+    arbol.mostrar_mensaje_descifrado();
+    
+    // CONSULTA
+    arbol.consulta_placa(1024);
+    arbol.consulta_placa(-10);
+    arbol.consulta_placa(661);
+
+    // BAJA
+    cout << "Placa que no se encuentra el el arbol" << endl;
+    arbol.baja_placa(-10);
+
+    cout << "Placa que se encuentra en el arbol" << endl;
+    arbol.baja_placa(661);
+    arbol.consulta_placa(661);
+
     return 0;
 }
