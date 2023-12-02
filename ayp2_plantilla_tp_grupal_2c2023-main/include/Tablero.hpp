ead3798c298d5efc6d058a7432fc68c23679f2dc
Eduardo Gervan <egervan@fi.uba.ar>
2023-12-02 17:09:15 -0300

    Agrego los archivos Tablero.hpp y Tablero.cpp

---
+++ ayp2_plantilla_tp_grupal_2c2023-main/include/Tablero.hpp
@@ -0,0 +1,60 @@
+#ifndef TABLERO_HPP
+#define TABLERO_HPP
+
+#include <string>
+#include "Grafo.hpp"
+
+const size_t CANT_CASILLEROS=81;
+const size_t ENTRADA=0;
+const size_t SALIDA=80;
+const size_t PESO_BASE=10;
+
+class Tablero {
+
+private:
+    Grafo* layout;
+    size_t ENTRADA;
+    size_t SALIDA;
+    size_t pos_jugador;
+    std::vector<size_t> pos_pyramid;
+    std::vector<size_t> paredes;
+
+//pre:-
+//pos: Desconecta la casilla del layout seteando las aristas a vertices adyacentes a infinito.
+void aislar_casilla(size_t casilla);
+
+//pre:-
+//pos: Conecta la casilla del layout seteando las aristas a vertices adyacentes a su peso base.
+void conectar_casilla(size_t casilla);
+
+//pre:-
+//pos: Aisla las casillas "pared" en base a la altura del nivel del arbol de placas.
+void set_layout(int altura);
+
+//pre:-
+//pos: Crea a lo sumo 2 pyramid heads y los ubica en vertices validos del layout.
+void crear_pyramid();
+
+//pre:-
+//pos: Quita a pyramid del tablero, restituyendo la casilla y seteando las aristas a vertices adyacentes a valor base.
+void eliminar_pyramid();
+
+//pre:-
+//pos: Inicia el grafo conectando los vertices a traves de aristas de peso base. (GONZA, si le pifie editalo)
+void pre_tablero();
+
+public:
+    //constructor
+    Tablero();
+
+    void iniciar_tablero();
+    
+    size_t actualizar_posicion();
+
+    void modificar_tablero(bool arma);
+
+    std::pair<std::vector<size_t>,int> camino_automatico();
+
+};
+
+#endif
